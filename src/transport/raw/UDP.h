435d2ef2089d0f3f0ec6a628b225f0ec9dedf957
Michael Sandstedt <michael.sandstedt@smartthings.com>
2020-10-08 16:13:20 -0500

    make sure CHIP_CONFIG_ENABLE_EPHEMERAL_UDP_PORT is observed
    This was being ignored in transport/UDP.h.  Fixing this allows
    CHIP_CONFIG_ENABLE_EPHEMERAL_UDP_PORT to be set in CHIPProjectConfig.h
    such that controllers acting in a client role will bind to an ephemeral
    port instead of the well-known CHIP server port.
    
    (cherry picked from commit f29595f08574c8b52533c9d96fa93b5d467c4baf)

--- src/transport/raw/UDP.h
+++ src/transport/raw/UDP.h
@@ -73,7 +73,11 @@ public:
 private:
     Inet::InetLayer * mLayer         = nullptr;                   ///< Associated inet layer
     Inet::IPAddressType mAddressType = Inet::kIPAddressType_IPv6; ///< type of listening socket
-    uint16_t mListenPort             = CHIP_PORT;                 ///< UDP listen port
+    #if CHIP_CONFIG_ENABLE_EPHEMERAL_UDP_PORT
+    uint16_t mListenPort             = 0;                     ///< UDP listen port
+    #else
+    uint16_t mListenPort             = CHIP_PORT;             ///< UDP listen port
+    #endif
     Inet::InterfaceId mInterfaceId   = INET_NULL_INTERFACEID;     ///< Interface to listen on
 };
 
